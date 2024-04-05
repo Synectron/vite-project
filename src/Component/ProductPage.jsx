9307d3335a3cea6969f6f35f84f36df6fe095e82
Synectron <146229549+ShubhamTech09@users.noreply.github.com>
2024-04-05 22:46:36 +0530

    useState

--- src/Component/ProductPage.jsx
+++ src/Component/ProductPage.jsx
@@ -1,6 +1,28 @@
+// import axios from "axios";
+// import { useEffect } from "react";
+
+import { useState } from "react";
+
 const ProductPage = (val) => {
-  console.log("val", val);
-  return <></>;
+  const [counter, setCounter] = useState(0);
+  //const [state, setState] = useState(initialState)
+  const [name, setName] = useState(null);
+
+  //let counter = 0;
+
+  const clickCounter = () => {
+    setCounter(counter + 1);
+    setName("Amrita");
+    console.log("adgs");
+  };
+
+  return (
+    <>
+      {counter}
+      {name}
+      <button onClick={clickCounter}>Click me</button>
+    </>
+  );
 };
 
 export default ProductPage;
@@ -8,3 +30,14 @@ export default ProductPage;
 //cognitive commplexity
 
 //closure + hoisting +lexical env
+
+// useEffect(() => {
+//   axios
+//     .get("https://localhost:3001/products")
+//     .then((response) => {
+//       console.log("response:", response.data);
+//     })
+//     .catch((error) => {
+//       console.log("error", error);
+//     });
+// }, []);
